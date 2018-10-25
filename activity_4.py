8c3eb2f98706fdfa2049cf7b1c8fb961649bf674
maryam <maryam-musalam@hotmail.com>
2018-10-25 15:06:27 +0300

    create do_post function

--- activity_4.py
+++ activity_4.py
@@ -23,4 +23,34 @@ form = '''<!DOCTYPE html>
     '''
 
 class ShowInfo(BaseHTTPRequestHandler):
+    def do_POST(self):
+        #Submit the form
+        length = int(self.headers.get('Content-length', 0))
+        info = self.rfile.read(length).decode()
+        param = parse_qs(info)
+        
+        
+        
+        if "name" in param and "city" in param and "enrollment" in param and "nanodegree" in param and "status" in param:
+            
+            name = param["name"][0]
+            city= param["city"][0]
+            enrollment = param["enrollment"][0]
+            nanodegree= param["nanodegree"][0]
+            status= param["status"][0]
+            
+            self.send_response(300)
+            self.send_header('Location' , '/')
+            self.end_headers()
+            
+            known = "'{}' is '{}' in '{}' studying '{}'. he/she is '{}' ".format(name ,enrollment,  city, nanodegree , status)
+            self.wfile.write(form.format(known).encode())
+        
+        else:
+            self.send_response(400)
+            self.send_header('content-type' , 'text/plain; charset=utf-8')
+            self.end_headers()
+            self.wfile.write("you can't leave empty feilds".encode())
+
+
 
