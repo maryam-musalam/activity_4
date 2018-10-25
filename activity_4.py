c759dfe25223478856ee09dbb73ace6903cc1bbb
maryam <maryam-musalam@hotmail.com>
2018-10-25 15:03:37 +0300

    create form variable holding the form

--- activity_4.py
+++ activity_4.py
@@ -2,4 +2,23 @@ from urllib.parse import parse_qs
 from http.server import BaseHTTPRequestHandler , HTTPServer
 
 memory = {}
+form = '''<!DOCTYPE html>
+    <title>Udacian</title>
+    <form method="POST" action="http://localhost:8000/">
+    <textarea name="name" placeholder="Enter the name"></textarea>
+    <br>
+    <textarea name="city" placeholder="Enter city"></textarea>
+    <br>
+    <textarea name="enrollment" placeholder="Enter the enrollment (ex:enrollment / didn't enroll)"></textarea>
+    <br>
+    <textarea name="nanodegree" placeholder="Enter the nanodegree(ex: FSND in sat with Ms. elham ):"></textarea>
+    <br>
+    <textarea name="status" placeholder="Enter status(ex:ontrack - not in track):"></textarea>
+    <br>
+    <button type="submit">Post it!</button>
+    </form>
+    <pre>
+    {}
+    </pre>
+    '''
 
